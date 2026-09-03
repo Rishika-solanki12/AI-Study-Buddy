a350a1da95f22d3f0afdfc563807d02ab1c39c00
Rishika-solanki12 <rishika121714@gmail.com>
2026-09-03 08:34:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -297,13 +297,14 @@ def response_to_text(response):
 # ==========================================================
 
 def remove_thinking(text):
+
     if not text:
         return ""
 
     text = str(text)
-s
+
     # Remove <think>...</think>
-        text = re.sub(
+    text = re.sub(
         r"<think\b[^>]*>.*?</think>",
         "",
         text,
@@ -325,8 +326,7 @@ s
         text
     )
 
-    return text.strip()
-# ==========================================================
+    return text.strip()# ==========================================================
 # SAFE LLM CALL
 # ==========================================================
 
